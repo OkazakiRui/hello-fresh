5a0e8d85e19831f99d01724ccb3113fbc9c7468a
OkazakiRui <okazaki200226@gmail.com>
2022-08-05 22:09:00 +0900

    feat: pageprops

---
+++ routes/greet/[name].tsx
@@ -0,0 +1,17 @@
+/** @jsx h  */
+import { h } from "preact";
+import { PageProps } from "$fresh/server.ts";
+
+export default function GreetPage(props: PageProps) {
+  const { name } = props.params;
+  console.log(props);
+  const propsString = JSON.stringify(props);
+
+  return (
+    <div>
+      <p>Greetings to you, {name}!</p>
+      <hr />
+      <small>{propsString}</small>
+    </div>
+  );
+}
